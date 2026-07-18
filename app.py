a65bdc116a3eea58ce10b3c8920ca25546de2bcc
Andre Ivo <andreivo637@gmail.com>
2026-07-18 13:02:01 -0300

    get clientes

--- app.py
+++ app.py
@@ -12,7 +12,30 @@ def get_conexao():
         use_pure=True,
     )
 
+@app.route("/clientes", methods=["GET"])
+def buscar_clientes():
+    mydb = get_conexao()
+    cur = mydb.cursor()
+    cur.execute("SELECT cli_codigo, cli_nome, cli_email, cli_criado_em FROM clientes")
+    tb_clientes = cur.fetchall()
+    clientes = list()
 
+    for cliente in tb_clientes:
+        clientes.append(
+            {
+                'id': cliente[0],
+                'nome': cliente[1],
+                'email': cliente[2],
+            }
+        )
+
+
+    return make_response(
+        jsonify(
+            dados=clientes,
+            mensagem = "lista de clientes.",
+        )
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
